3ed91d92df1897ea22502eda4b413d7f0a42415c
Prabhat <672099@cognizant.com>
2018-03-07 11:38:59 +0530

    Broadcast feature Added

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,22 @@
+//client side endpoint -- exposed using io()
+
 "use strict";
 
 let socket = io(); //initiates a request from the client to the server to open a websocket and keep it open
             
 socket.on('connect',()=>{
     console.log('Connected to server.');
-    socket.emit("getEmail",{
-        to:"abc@dummy.org",
-        text:"Hello there"
-    });
+    // socket.emit("createMessage",{
+    //     to:"abc@dummy.org",
+    //     text:"Hello there"
+    // });
 });
+
 
 socket.on('disconnect',()=>{
     console.log('Disconnected from server.');
 });
 
-socket.on('newEmail',(email)=>{
-    console.log("New Email Received",email);
+socket.on('newMessage',(message)=>{
+    console.log("New Message Received",message);
 });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+//server side endpoint
+
 "use strict";
 
 const path = require("path");
@@ -12,6 +14,7 @@ const port = process.env.PORT||3000;
 let publicPath = path.join(__dirname,"../public");
 let server = http.createServer(app);
 let io = socketIO(server);
+
 //middleware --> helps serve up the public folder
 app.use(express.static(publicPath));
 
@@ -19,15 +22,24 @@ console.log(publicPath);
 
 io.on('connection',(socket)=>{
     console.log("New user connected.");
+    
+    let today = new Date();
+    let date = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
+    let time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
 
-    socket.emit('newEmail',{
-        from:"dummyclient@abcmail.com",
-        text:"Hello",
-        completedAt:123
-    });
+    // socket.emit('newMessage',{
+    //     from:"dummyclient@abcmail.com",
+    //     text:"Hello",
+    //     completedAt: date+" "+time+"Hrs"
+    // });
 
-    socket.on('getEmail',(newEmail)=>{
-        console.log("New Email Sent",newEmail)
+    socket.on('createMessage',(newMessage)=>{
+        console.log("New Message Received",newMessage);
+        io.emit('newMessage',{
+            from:newMessage.from,
+            text:newMessage.text,
+            createdAt: date+" "+time+"Hrs"
+        });
     })
 
     socket.on('disconnect',(socket)=>{
